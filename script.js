636bf9dda9a4f42e50cddf68b0d1b3cd956d6e18
lalafatima33 <82950609+lalafatima33@users.noreply.github.com>
2023-03-13 20:17:43 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const title = document.querySelector('h2')
+const timeField = document.querySelector('b')
+
+let timer = Math.floor(Date.now () /1000)- 1640099567 + 7200;
+timeField.textContent = timer.
